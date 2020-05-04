bb78dbd5219b7e45708cb96cccafec99c2dfd04c
Matan Bouganim <matan@bouganim.com>
2020-05-04 21:47:38 +0300

    all my work updated now that we have started JS ! SO EXCITED

---
+++ week_3/day1/dailychallenge.js
@@ -0,0 +1,12 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+array.splice(0,1)
+array.push("Kiwi")
+array.splice(0,1)
+var backwardsArray = array.reverse() 
+
+
+
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+let vitC = array2[1,1]
+console.log(vitC)
